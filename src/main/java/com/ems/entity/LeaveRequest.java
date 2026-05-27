5a8fd93e3e8839070eca4185e9c1ae8d439c885c
rajkhade44-web <rajkhade44@gmail.com>
2026-05-27 19:54:39 +0530

    Implemented LeaveRequest entity with approval

---
+++ src/main/java/com/ems/entity/LeaveRequest.java
@@ -0,0 +1,57 @@
+package com.ems.entity;
+
+import java.time.LocalDate;
+
+import com.ems.enums.LeaveStatus;
+import com.ems.enums.LeaveType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "leave_requests")
+public class LeaveRequest {
+    @Enumerated(EnumType.STRING)
+    @Column(name = "leave_type", nullable = false)
+    private LeaveType leaveType;
+
+    @Column(nullable = false)
+    private String reason;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date",nullable = false)
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LeaveStatus status;
+
+    @Column(name = "rejection_reason")
+    private String rejectionReason;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "approved_by")
+    private User approvedBy;
+}
